8ee2cf790ba8657ba7f3fabe012391bf7a0e7b2e
FloWinkler <florian.michael.winkler@hotmail.com>
2023-06-28 21:40:58 +0200

    changd designe

--- src/Components/Board/Board.styled.js
+++ src/Components/Board/Board.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Title = styled.h1`
     width: 100vw;
     height: 10vh;
-    margin-top: 0;
+    margin-top: 20px;
     margin-bottom: 0;
     font-size: 2.75vw;
     color: #fde047;
